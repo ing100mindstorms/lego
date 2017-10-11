dd88f7df07e8ef3035b28d47d7c1b7e6f3021cde
neneu <f.wabakken@gmail.com>
2017-10-11 15:26:35 +0200

    structuring

--- MineFunksjoner/Derivation.m
+++ MineFunksjoner/Derivation.m
@@ -1,5 +1,9 @@
+% Secant derivation
+% Derivation(FunctionValues, TimeStep) use the
+% following values of the discrete index k-1:
+% - FunctionValues (the function values to be derivated)
+% - TimeStep (time difference between discrete index k and k-1)
+% to calculate the derivation value at discrete index k-1.
 function [Secant] = Derivation(FunctionValues, TimeStep)
-
-%%%%
     Secant = (FunctionValues(1) - FunctionValues(2))/TimeStep;
 end
